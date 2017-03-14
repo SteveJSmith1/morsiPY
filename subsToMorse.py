0a3f541fbffd24637d0f9a2727c76614a6c39ed9
SteveJSmith1 <stevejsmith567@gmail.com>
2017-03-14 11:13:59 +0000

    Updates to subsToMorse.py
    - Removed parseSRT function and replaced with a call to srtTools
    
    - Rewrite of processWords to collect the subtitle lines in a different
    way
    
    - Removed function dotWavFile and replaced with a call to MorseAudio.py
    
    - Inserted doc strings for program and functions

--- subsToMorse.py
+++ subsToMorse.py
@@ -1,132 +1,157 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 11 14:04:38 2017
+Created on Tue Mar 14 09:59:03 2017
 
 @author: SteveJSmith1
-"""
 
-def parseSRT(file):
-    from srtTools import srtParse
-    parsed = srtParse(file, word_lines=False, times=False, tokens=False, tokens2=False)
-    return parsed
+subsToMorseBeta.py
+
+This program parses a .srt file, converts
+the lines of text to morse code and then
+creates an audio file of morse code.
+
+usage: 
+    subsToMorse(file, save_file)
     
-
-
+    where file is the srt file to process
+    and save_file is the filename of the output
+"""
+import re
+import datetime
+from MorseAudio import writeWav
+from MorseAudio import processDot
+from MorseAudio import dotFile
+from MorseAudio import getDotEquivs
+from MorseTranslator import morseCoder
+from srtTools import srtParse
 
 def extractTimes(parsed):
-    import re
+    """
+    Extracts the start and end time of each subtitle
+    group from the parsed file
+    """
+    # find the lines that contain times
     times = [l for l in parsed if re.search(r'^[0-9][0-9]:', l)]
+    # extract the start and end times
     start_times = [time[:12] for time in times]
     end_times = [time[-12:] for time in times]
     return start_times, end_times
+
     
-
-
 def getTimedelta(times):
-    import datetime
-
+    """
+    This function creates time deltas for the
+    passed list of times
+    """
     time_deltas = []
+    # extracting the time periods from the times
     for i in range(len(times)):
         h = int(times[i][:2])
         m = int(times[i][3:5])
         s = int(times[i][6:8])
         ms = int(times[i][-3:])
+        #create timedelta object for each
         time_deltas.append(datetime.timedelta(seconds=s, milliseconds=ms, minutes=m, hours=h))
     return time_deltas
     
 
 def subtitleTimings(starts_td, ends_td):
-    
+    """
+    Extracts the length a subtitle is displayed
+    on screen in milliseconds when passed
+    the start and end timedeltas
+    """
     sub_ms = []
     for i in range(len(starts_td)):
         delta = (ends_td[i]-starts_td[i]).total_seconds()*1000
         sub_ms.append(round(delta))
     return sub_ms
 
-
-
-
+    
 def silenceMS(starts_td, ends_td):
-    import datetime
+    """
+    This function uses the time deltas to
+    extract periods where subtitles are not 
+    on screen, i.e. silence
+    """
     silence = []
+    # first period of silence
     silence.append(starts_td[0] - datetime.timedelta(0, 0, 0))
     for i in range(1,len(starts_td)):
         silence.append(starts_td[i] - ends_td[i - 1])
      
     silence_ms = [round(i.total_seconds()*1000) for i in silence]
     return silence_ms
-    
-
-
+ 
 
 def processWords(parsed):
-    import re
-
+    """
+    This function fetches the number of lines
+    on screen for each time period and creates
+    a single string
+    """
+    # finds all 'word lines' in parsed text
     words = [l for l in parsed if not re.search(r'^[0-9][0-9]:', l) and not re.search(r'^[0-9]+$', l)]
-    #append blank line as loop depends upon it
-    words.append('')
-
-    split = []
-    for i in range(2,len(words)):
-        s = []
-        if words[i] == '' and words[i-1] != '' and words[i-2] !=2:
-            test = words[i-2] + ' ' + words[i-1]
-            s.append(test)
-        elif words[i] == '' and words[i-1]:
-            s.append(words[i-1])
-        split.append(s)
-        
-    processed = [line for line in split if line != []]
-    return processed
+    # replaces lines of '' with \\split\\
+    for i in range(len(words)):
+        if words[i] == '':
+            words[i] = '\\split\\'
     
+    # join the lines together
+    joined = ' '.join(words)
+    # split back into lines using
+    processed = joined.split('\\split\\')
+    return processed
 
-
-#====================================================
-
-
-
-from MorseTranslator import morseCoder
 
 def encodeList(processed):
+    # pass the list to morseCoder
+    # full=False strips punctuation
     morse = [morseCoder(str(l), encode=True, full=False) for l in processed]
     return morse
     
-
-
-from MorseAudio import getDotEquivs
-
+    
 def dotEquivs(morse):
+    """
+    Fetches the number of dot equivalents
+    for each line of morse
+    """
+    
+    # pass the list to getDotEquivs
     dot_equivs = [getDotEquivs(m) for m in morse]
     return dot_equivs
 
 
-
-
-def dotWavFile():
-    return 'dot1.wav'
-    
-
 def dotMS(sub_ms, dot_equivs):
+    """
+    Calculates the required length of an
+    audible 'dot'
+    """
     len_dot_ms = []
     for i in range(len(sub_ms)):
         len_dot_ms.append(sub_ms[i]/dot_equivs[i])
     return len_dot_ms
-    
 
-
-from MorseAudio import processDot
 
 def nFrames(len_dot_ms):
-    framerate, _ , length, _ = processDot(dotWavFile())
+    """
+    calculates the number of frames required
+    to return from the dot file
+    """
+    
+    framerate, _ , length, _ = processDot(dotFile())
     frames_per_ms = framerate/length
 
     nframes = [round(frames_per_ms*l) for l in len_dot_ms]
     return nframes
-    
 
 
 def dotBytes(nframes):
-    file = dotWavFile()
+    """
+    Creates a bytes object representing the 'dot'
+    """
+    
+    file = dotFile()
     import wave
     dot_bytes = []
     for n in nframes:
@@ -135,12 +160,13 @@ def dotBytes(nframes):
         dot_file.close()
         
     return dot_bytes
-    
-
-
 
 
 def compileSoundset(dot_bytes):
+    """
+    returns dot, dash and silences as bytes
+    objects
+    """
     sound_sets = []
     for i in range(len(dot_bytes)):
         dash = dot_bytes[i]*3
@@ -152,15 +178,14 @@ def compileSoundset(dot_bytes):
         
         sound_sets.append(ss)
     return sound_sets
-    
-
 
 
 def morseBytes(morse, sound_sets):
     
     
     """
-    
+    Compiles the bytes for each line of morse code
+    using each sound set
     """
     
     morse_compiled_bytes = []
@@ -177,38 +202,54 @@ def morseBytes(morse, sound_sets):
                  ' /' : dot + silence3
                  }
         char_pairs = [(morse[i][j] + morse[i][j + 1]) for j in range(len(morse[i])-1)]
-     
+        
+        # bytearray is faster
         complete_bytes = bytearray()
-    
+        # applies the correct soundset for each
+        # character pair
         for pair in char_pairs:
             if pair in rules:
                 complete_bytes += rules.get(pair)
         morse_compiled_bytes.append(bytes(complete_bytes))
     return morse_compiled_bytes
-    
-
-
+ 
 
 def silenceBytes(silence_ms):
-    framerate, _, _, _ = processDot(dotWavFile())
+    """
+    Creates a byte object of zeros
+    to be encoded as silence
+    """
+    
+    framerate, _, _, _ = processDot(dotFile())
+    
     frames_per_ms = framerate/1000
     silence_bytes = []
+    # muliplier by 4 as 4 bits in a ms
     for val in silence_ms:
         sb = bytes(round(frames_per_ms*4*val))
         silence_bytes.append(sb)
         
     return silence_bytes
-    
 
 
 def completeBytes(silence_bytes, morse_bytes):
+    """
+    compiles the bytes object of silence and morse
+    """
+    #bytearray is much faster
     complete = bytearray()
     for i in range(len(silence_bytes)):
         complete += silence_bytes[i]
         complete += morse_bytes[i]
+    # convert back to a bytes object
     return bytes(complete)
- 
+
+    
 def processTimes(parsed):
+    """
+    The functions required to process the times
+    are collected here
+    """
     start_times, end_times = extractTimes(parsed)
     starts_td = getTimedelta(start_times)
     ends_td = getTimedelta(end_times)
@@ -216,12 +257,14 @@ def processTimes(parsed):
     silence_ms = silenceMS(starts_td, ends_td)
     return sub_ms, silence_ms, ends_td
     
-
- 
-def subsToMorse(file, save_file):
     
-
-    parsed = parseSRT(file)
+def subsToMorse(file, save_file):
+    """
+    The main function call
+    """
+    
+    parsed = srtParse(file, word_lines=False, times=False, tokens=False, tokens2=False)
+    
     sub_ms, silence_ms, ends_td = processTimes(parsed)
     
     processed = processWords(parsed)
@@ -234,36 +277,26 @@ def subsToMorse(file, save_file):
     sound_sets = compileSoundset(dot_bytes)
     morse_bytes = morseBytes(morse, sound_sets)
     
-    
     silence_bytes = silenceBytes(silence_ms)
-    
     
     complete_bytes = completeBytes(silence_bytes, morse_bytes)
     
-    
-    from MorseAudio import writeWav
-    
-    _,_,_,params = processDot(dotWavFile())
+    _,_,_,params = processDot(dotFile())
        
     writeWav(complete_bytes, save_file, params)
     
-    afl = audio_file_length = len(complete_bytes)/44100/4
+    audio_file_length = len(complete_bytes)/44100/4
     
-    sl = subtitle_length = ends_td[-1].total_seconds()
-    
+    subtitle_length = ends_td[-1].total_seconds()
+    afl, sl = audio_file_length, subtitle_length
     lep = (afl-sl)/sl*100
     
-    
     return print('The error in length is %g%%' % lep)
-    
-       
 
-    
-    
-    
+"""    
 file = r'D:\Data\Subs\24\Season 1\24.1x01.12am-1am.DVDRip.XViD-FoV.EN.srt'
 subsToMorse(file, '24E1Morse.wav')
-
+"""
 
 
 
